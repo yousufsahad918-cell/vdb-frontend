26b8c8f8c3fd0bb519c00d6c45050c218433894e
Yousuf Sahad <lanetwork.in@gmail.com>
2026-04-26 15:08:14 +0530

    fix: robots.txt disallow sitemap from search, fix stray folder

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,18 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/admin",
+          "/api/",
+          "/sitemap.xml",
+        ],
+      },
+    ],
     sitemap: "https://vapeinbangalore.in/sitemap.xml",
+    host: "https://vapeinbangalore.in",
   };
 }
