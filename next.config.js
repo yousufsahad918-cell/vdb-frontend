dd763c2011770f9fe68397a1338e10f84f41122d
ashilksdofficial <ashilksdofficial@gmail.com>
2026-08-12 21:18:14 +0530

    fix: add www to non-www redirect to fix duplicate canonical GSC warning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.vapedeliverybangalore.com' }],
+        destination: 'https://vapedeliverybangalore.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
